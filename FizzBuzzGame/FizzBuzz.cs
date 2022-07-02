110b239d117439f180abd40d6ce78d8054f7b323
RokerDev <rokerdev91@gmail.com>
2022-07-02 23:40:43 +0200

    add detailed logic

--- FizzBuzzGame/FizzBuzz.cs
+++ FizzBuzzGame/FizzBuzz.cs
@@ -1,9 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzzGame
 {
     class FizzBuzz
     {
-        
+        internal int TakeInput()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int num))
+                {
+                    Console.WriteLine($"I need number between {int.MinValue}...{int.MaxValue}. Try again.");
+                    continue;
+                }
+                return num;
+            }
+        }
+
+        internal string Result(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+                return num.ToString();
+        }
+
+        internal bool PlayAgain()
+        {
+            var list = new List<string>() { "Yes", "yes", "y", "Y" };
+            var userInput = Console.ReadLine();
+            if (list.Contains(userInput))
+            {
+                Console.Clear();
+                return true;
+            }
+            else
+                return false;
+        }
     }
 }
